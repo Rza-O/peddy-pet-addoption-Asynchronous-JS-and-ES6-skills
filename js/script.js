840f95b499122dd87e558d5ef742fcc3d75a59ef
appoclayptor <shahreza1414@gmail.com>
2024-10-07 03:10:25 +0600

    Added optional Chaining for null and undefined

--- js/script.js
+++ js/script.js
@@ -13,14 +13,23 @@ const showAllPets = (pets) => {
         card.innerHTML = `
                         <div class="p-5"><img class="object-cover rounded-lg" src=${pet.image} alt=""></div>
                         <div class="p-5 flex flex-col gap-2">
-                            <h3 class="text-2xl font-extrabold">${pet.pet_name}</h3>
-                            <p><i class="fa-solid fa-grip"></i> Breed: ${pet.breed}</p>
-                            <p><i class="fa-regular fa-calendar"></i> Birth: ${pet.date_of_birth}</p>
-                            <p><i class="fa-solid fa-mercury"></i> Gender: ${pet.gender}</p>
-                            <p><i class="fa-solid fa-dollar-sign"></i> Price: ${pet.price}</p>
+                            <h3 class="text-2xl font-extrabold">${pet?.pet_name ?? 'Not Mentioned'}</h3>
+                            <p><i class="fa-solid fa-grip"></i> Breed: ${pet?.breed ?? 'Not Mentioned'}</p>
+                            <p><i class="fa-regular fa-calendar"></i> Birth: ${pet?.date_of_birth ?? 'Not Mentioned'}</p>
+                            <p><i class="fa-solid fa-mercury"></i> Gender: ${pet?.gender ?? 'Not Mentioned'}</p>
+                            <p><i class="fa-solid fa-dollar-sign"></i> Price: ${pet?.price ?? 'Not Mentioned$'}</p>
+                        </div>
+                        <div>
                         </div>
         `;
         cardContainer.append(card);
     });
 }
 loadAllPets();
+// const card = `
+//   <div class="card">
+//     <h2>${user?.name ?? 'Unknown Name'}</h2>
+//     <p>${user?.age ?? 'Age not available'}</p>
+//     <p>${user?.job ?? 'Job not specified'}</p>
+//   </div>
+// `;
